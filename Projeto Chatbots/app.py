e17f4a5d92ac28bf5fef2b5428dfbb6f220f9c26
Vitor_Melo_DEVFullStcak <144180128+VitorMeloR@users.noreply.github.com>
2024-01-21 11:48:23 -0300

    projeto finalalizado

--- Projeto Chatbots/app.py
+++ Projeto Chatbots/app.py
@@ -76,6 +76,11 @@ def monof():
     return render_template('monofasico.html')
 
 
+@app.route('/trifasico')
+def trif():
+    return render_template('trifasico.html')
+
+
 # Rota para lidar com o envio do formulário
 @app.route('/buscar_monof', methods=['POST'])
 def buscar_monof():
@@ -183,17 +188,126 @@ def dados_encontrados_monofasico():
     fio_auxiliar = request.args.get('fio_auxiliar')
     ligacao = request.args.get('ligacao')
 
-    dados_motor = buscar_dados_motor_monof(marca_motor, potencia, modelo, voltagem, amperagem, rotacao, pacote, )
+    dados_motor = buscar_dados_motor_monof(marca_motor, potencia, modelo, voltagem, amperagem, rotacao, pacote)
 
     return render_template('dados_encontrados.html', dados_motor=dados_motor)
+
+
+@app.route('/buscar_trif', methods=['POST'])
+def buscar_trif():
+    marca_motor = request.form['marca_motor']
+    potencia = request.form['potencia']
+    modelo = request.form['modelo']
+    voltagem = request.form['voltagem']
+    amperagem = request.form['amperagem']
+    rotacao = request.form['rotacao']
+    pacote = request.form['pacote']
+    cod_busca = str(marca_motor[0:3] + potencia + modelo.strip() + voltagem[0:3] + amperagem[0:3] + voltagem[4:] +
+                    amperagem[4:] + rotacao + pacote)
+    print(cod_busca)
+
+    dados_motor_trif = buscar_dados_motor_trif(cod_busca, marca_motor, potencia, modelo, voltagem, amperagem, rotacao,
+                                           pacote)
+
+    if dados_motor_trif:
+        return render_template('dados_motor_tri.html', dados_motor_trif=dados_motor_trif)
+    else:
+        return render_template('dados_trif.html', dados=request.form)
+
+
+def buscar_dados_motor_trif(cod_busca,  marca_motor, potencia, modelo, voltagem, amperagem, rotacao, pacote):
+    conn = sqlite3.connect('trifasico.db')
+    cursor = conn.cursor()
+
+    # Buscar dados na tabela motores usando uma consulta parametrizada
+    cursor.execute('''
+        SELECT * FROM motores
+        WHERE cod_busca = ? OR
+              marca_motor = ? OR
+              potencia = ? OR
+              modelo = ? OR
+              voltagem = ? OR
+              amperagem = ? OR
+              rotacao = ? OR
+              pacote = ? OR
+              bobina_grupo = ? OR
+              grupo_fase = ? OR
+              camada = ? OR
+              passe = ? OR
+              espiras = ? OR
+              fio = ? OR
+              ligacao = ?
+    ''', (cod_busca, '', '', '', '', '', '', '', '', '', '', '', '', '', ''))
+
+    dados_motor_trif = cursor.fetchall()
+
+    conn.close()
+
+    return dados_motor_trif
+
+
+@app.route('/adicionar_trif', methods=['POST'])
+def adicionar_trif():
+    marca_motor = request.form['marca_motor']
+    potencia = request.form['potencia']
+    modelo = request.form['modelo']
+    voltagem = request.form['voltagem']
+    amperagem = request.form['amperagem']
+    rotacao = request.form['rotacao']
+    pacote = request.form['pacote']
+    bobina_grupo = request.form['bobina_grupo']
+    grupo_fase = request.form['grupo_fase']
+    camada = request.form['camada']
+    passe = request.form['passe']
+    espiras = request.form['espiras']
+    fio = request.form['fio']
+    ligacao = request.form['ligacao']
+    cod_busca = str(marca_motor[0:3] + potencia + modelo.strip() + voltagem[0:3] + amperagem[0:3] + voltagem[4:] +
+                    amperagem[4:] + rotacao + pacote)
+
+    conn = sqlite3.connect('trifasico.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO motores (cod_busca, marca_motor, potencia, modelo, voltagem, amperagem, rotacao, pacote, bobina_grupo, grupo_fase, 
+        camada, passe, espiras, fio, ligacao)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (cod_busca, marca_motor, potencia, modelo, voltagem, amperagem, rotacao, pacote, bobina_grupo, grupo_fase, camada,
+          passe, espiras, fio, ligacao))
+
+    conn.commit()
+    conn.close()
+
+    return redirect('/')
+
+
+@app.route('/dados_encontrados_trifasico')
+def dados_encontrados_trifasico():
+    marca_motor = request.args.get('marca_motor')
+    potencia = request.args.get('potencia')
+    modelo = request.args.get('modelo')
+    voltagem = request.args.get('voltagem')
+    amperagem = request.args.get('amperagem')
+    rotacao = request.args.get('rotacao')
+    pacote = request.args.get('pacote')
+    bobina_grupo = request.args.get('bobina_grupo')
+    grupo_fase = request.args.get('grupo_fase')
+    camada = request.args.get('camada')
+    passe = request.args.get('passe')
+    espiras = request.args.get('espiras')
+    fio = request.args.get('fio')
+    ligacao = request.args.get('ligacao')
+
+    dados_motor_trif = buscar_dados_motor_trif(marca_motor, potencia, modelo, voltagem, amperagem, rotacao, pacote)
+
+    return render_template('dados_encontrados_trif.html', dados_motor_trif=dados_motor_trif)
 
 
 @app.route('/verifficar_tabela')
 def verificar_tabela():
     criar_tabela_monofasico()
+    criar_tabela_trifasico()
     return 'Tabela criada com sucesso!'
-
-
 
 
 if __name__ == '__main__':
